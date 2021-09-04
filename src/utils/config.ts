91c1e6b96e139dea0fda9d17746e6966517fdfd4
Paulo Cândido <paulo.candido@g.globo>
2021-09-03 23:59:55 -0300

    feat: provide email configuration

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import dotenv from 'dotenv'
 
 function getSecret(key: string): string | undefined {
   const secretsPath = process.env.SECRECT_PATH || '/run/secrets'
@@ -26,7 +27,14 @@ function getIntConfig(key: string, defaultValue: number) {
   return parseInt(value)
 }
 
+function getBoolConfig(key: string, defaultValue: boolean) {
+  const value = getConfig(key, defaultValue.toString())
+  return value === 'true'
+}
+
 function genConfig() {
+  dotenv.config()
+
   return {
     app: {
       port: getIntConfig('PORT', 5000),
@@ -41,20 +49,27 @@ function genConfig() {
       passwordRecovery: {
         expiresInMinutes: getIntConfig('PASSWORD_RECOVERY_EXPIRES_IN_MINUTES', 24 * 60),
         resetUrl: getConfig('PASSWORD_RESET_URL', 'https://domain.com/password-reset'),
-        queueName: getConfig('PASSWORD_RECOVERY_RABBITMQ_QUEUE', 'send-email'),
       },
     },
     rabbitmq: {
       host: getConfig('PASSWORD_RECOVERY_RABBITMQ_HOST', 'localhost'),
     },
     mongodb: {
-      url: getConfig('MONGO_URL', 'mongodb://localhost:27017/ms-account'),
+      url: getConfig('MONGO_URL', 'mongodb://localhost:27017/spotify'),
     },
     s3: {
       accessKeyId: getConfig('S3_ACCESS_KEY_ID', 'accessKeyId'),
       secretAccessKey: getConfig('S3_SECRET_ACCESS_KEY', 'secretAccessKey'),
       endpoint: getConfig('S3_ENDPOINT', 'http://localhost:4569'),
       bucketName: getConfig('S3_ACCOUNT_IMAGE_BUCKET', 'account-image'),
+    },
+    email: {
+      host: getConfig('EMAIL_HOST', ''),
+      port: getIntConfig('EMAIL_PORT', 465),
+      secure: getBoolConfig('EMAIL_SECURE', true),
+      sender: getConfig('EMAIL_SENDER', 'Spotify'),
+      user: getConfig('EMAIL_USER', ''),
+      password: getConfig('EMAIL_PASSWORD', ''),
     },
   }
 }
