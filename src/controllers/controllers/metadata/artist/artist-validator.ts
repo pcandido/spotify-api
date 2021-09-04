4f253ccf9ec81c7664d6ae62a9e0caedfa218d3e
Paulo Cândido <paulo.candido@g.globo>
2021-09-04 13:37:16 -0300

    feat: create the artist validator

---
+++ src/controllers/controllers/metadata/artist/artist-validator.ts
@@ -0,0 +1,9 @@
+import { MissingParamValidator, ValidatorComposite } from '@controllers/helpers/validation'
+import { ImageFieldValidator } from '@controllers/helpers/validation/image-field-validator'
+import { Validator } from '@controllers/protocols'
+
+export const artistValidator = (): Validator => new ValidatorComposite([
+  new MissingParamValidator('name'),
+  new MissingParamValidator('image'),
+  new ImageFieldValidator('image', 10),
+])
---
+++ src/controllers/controllers/metadata/artist/artist-validator.unit.test.ts
@@ -0,0 +1,48 @@
+import { MissingParamError, InvalidParamError } from '@controllers/errors'
+import { Validator, MultiPartFile } from '@controllers/protocols'
+import { artistValidator } from './artist-validator'
+
+interface SutTypes {
+  sut: Validator
+}
+
+const makeSut = (): SutTypes => {
+  const sut = artistValidator()
+  return { sut }
+}
+
+const makeImage = (): MultiPartFile => ({
+  originalName: 'any_name',
+  size: 500,
+  mimeType: 'image/jpg',
+  buffer: Buffer.from('any_image', 'utf-8'),
+})
+
+describe('ArtistValidator', () => {
+
+  it('should throw if no name is provided', async () => {
+    const { sut } = makeSut()
+    const givenRequest = { image: makeImage() }
+    expect(() => sut.validate(givenRequest)).toThrow(new MissingParamError('name'))
+  })
+
+  it('should throw if no image is provided', async () => {
+    const { sut } = makeSut()
+    const givenRequest = { name: 'any name' }
+    expect(() => sut.validate(givenRequest)).toThrow(new MissingParamError('image'))
+  })
+
+  it('should throw if mime type is not image', async () => {
+    const { sut } = makeSut()
+    const givenRequest = { name: 'any name', image: makeImage() }
+    givenRequest.image.mimeType = 'application/json'
+    expect(() => sut.validate(givenRequest)).toThrow(new InvalidParamError('image'))
+  })
+
+  it('should not throw if no validator throws', () => {
+    const { sut } = makeSut()
+    const givenRequest = { name: 'any name', image: makeImage() }
+    expect(() => sut.validate(givenRequest)).not.toThrow()
+  })
+
+})
